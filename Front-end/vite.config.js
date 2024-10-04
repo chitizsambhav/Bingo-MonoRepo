1832ba74056accca24b6cc199025828941facfb5
chitizsambhav <chitizsambhav23@gmail.com>
2024-10-04 11:49:37 +0530

    Made changes to vite.config.js to split the module into smaller chunks

--- Front-end/vite.config.js
+++ Front-end/vite.config.js
@@ -4,4 +4,25 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Ensure @splinetool is in its own chunk
+          if (id.includes('node_modules/@splinetool')) {
+            return 'splinetool';
+          }
+
+          // General chunking logic for other dependencies
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString();
+          }
+        }
+      }
+    }
+  }
 })
