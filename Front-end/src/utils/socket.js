84aa06cc1b7893b358e3e3ab51625194a7bebde3
chitizsambhav <chitizsambhav23@gmail.com>
2024-10-08 00:35:13 +0530

    Changed the Backend API URI to support env variable

--- Front-end/src/utils/socket.js
+++ Front-end/src/utils/socket.js
@@ -1,7 +1,6 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = 'http://localhost:3000'
-// import.meta.env.VITE_API_URL;
+const SOCKET_URL = import.meta.env.VITE_API_URL;
 
 const socket = io(SOCKET_URL, { autoConnect: true });
 
